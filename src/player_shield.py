cafc70bd3bf5846633b94312a062043230f09d21
Jon Jennemann <github@jonf.xyz>
2025-12-08 21:46:25 +0100

    Fixed variable name

--- src/player_shield.py
+++ src/player_shield.py
@@ -3,7 +3,8 @@ from pygame import Vector2
 from player_shield_polygon import player_shield_polygon
 from constants import PLAYER_SHIELD_MAX, \
     PLAYER_SHIELD_RADIUS, \
-    PLAYER_SHIELD_RIPPLE_MAX
+    PLAYER_SHIELD_RIPPLE_MAX, \
+    PLAYER_SHIELD_COOLDOWN
 
 
 class player_shield(entity):
@@ -14,15 +15,13 @@ class player_shield(entity):
         self.collideable = True
         self.use_physics = True
         self.radius = PLAYER_SHIELD_RADIUS
-        self.first_frame = True
         self.player = None
         self.value = PLAYER_SHIELD_MAX
-        self.collision_list = set()
+        self.collision_list = []
 
     def update(self):
-        if self.first_frame:
+        if not self.player:
             self.player = self.game.ent_manager.get_entity("player")
-            self.first_frame = False
         self.velocity = self.player.velocity
         if self.value == 0:
             self.polygon.show = False
@@ -34,13 +33,13 @@ class player_shield(entity):
             self.collideable = True
             self.use_physics = True
             self.player.collideable = False
-
+        for timer in self.collision_list[:]:
+            if timer["timer"] > 0:
+                timer["timer"] -= self.game.dt
 
     def on_physics_enter(self, entity):
         if self.value > 0:
-            if not entity.id in self.collision_list:
-                pass
-                #self.collision_list.add([entity.id, 0])
+            self.collision_list.append({"id": entity.id, "timer": PLAYER_SHIELD_COOLDOWN})
             self.value -= 10
             direction = entity.position - self.position
             direction = Vector2.normalize(direction)
@@ -53,7 +52,16 @@ class player_shield(entity):
             direction = Vector2.normalize(direction)
             self.polygon.ripple_direction.append(direction)
             self.polygon.ripple = PLAYER_SHIELD_RIPPLE_MAX
+        for e in self.collision_list:
+            if e["id"] == entity.id:
+                if e["timer"] <= 0:
+                    self.value -= 10
+                    e["timer"] = PLAYER_SHIELD_COOLDOWN
 
 
+    def on_physics_exit(self, entity):
+        for e in self.collision_list:
+            if e["id"] == entity.id:
+                self.collision_list.remove(e)
 
 
