5e0e90156714c3c76618fb97b6eba918bd208902
Jon Jennemann <github@jonf.xyz>
2025-12-09 08:02:38 +0100

    shield now recharges when down

--- src/player_shield.py
+++ src/player_shield.py
@@ -1,8 +1,11 @@
 from entity import entity
 from pygame import Vector2
 from player_shield_polygon import player_shield_polygon
-from constants import PLAYER_SHIELD_MAX, \
+from constants import PLAYER_RADIUS, \
+    PLAYER_SHIELD_FADE_TIME, \
+    PLAYER_SHIELD_MAX, \
     PLAYER_SHIELD_RADIUS, \
+    PLAYER_SHIELD_RECHARGE_TIME, \
     PLAYER_SHIELD_RIPPLE_MAX, \
     PLAYER_SHIELD_COOLDOWN
 
@@ -17,25 +20,42 @@ class player_shield(entity):
         self.radius = PLAYER_SHIELD_RADIUS
         self.player = None
         self.value = PLAYER_SHIELD_MAX
+        self.recharge_timer = 0
         self.collision_list = []
 
     def update(self):
         if not self.player:
             self.player = self.game.ent_manager.get_entity("player")
         self.velocity = self.player.velocity
+        self.position = self.player.position
         if self.value == 0:
-            self.polygon.show = False
+            if self.radius <= PLAYER_RADIUS:
+                self.polygon.show = False
+            else:
+                self.radius -= (self.game.dt / PLAYER_SHIELD_FADE_TIME) * PLAYER_SHIELD_RADIUS
             self.collideable = False
             self.use_physics = False
             self.player.collideable = True
+            self.recharge_shield(self.game.dt)
         else:
             self.polygon.show = True
             self.collideable = True
             self.use_physics = True
             self.player.collideable = False
+            if self.radius < PLAYER_SHIELD_RADIUS:
+                self.radius += (self.game.dt / PLAYER_SHIELD_FADE_TIME) * PLAYER_SHIELD_RADIUS
+                if self.radius > PLAYER_SHIELD_RADIUS:
+                    self.radius = PLAYER_SHIELD_RADIUS
         for timer in self.collision_list[:]:
             if timer["timer"] > 0:
                 timer["timer"] -= self.game.dt
+
+    def recharge_shield(self, dt):
+        if self.value == 0:
+            if self.recharge_timer < PLAYER_SHIELD_RECHARGE_TIME:
+                self.recharge_timer += dt
+                if self.recharge_timer >= PLAYER_SHIELD_RECHARGE_TIME:
+                    self.value = PLAYER_SHIELD_MAX
 
     def on_physics_enter(self, entity):
         if self.value > 0:
