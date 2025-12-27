8c2338cfdf82f00cd23b519990533a3f6bc1d4d1
Jon Jennemann <github@jonf.xyz>
2025-12-27 18:24:30 +0100

    remove debug prints

--- src/main_room.py
+++ src/main_room.py
@@ -8,8 +8,6 @@ class main_room(room):
     def load(self):
         self.game.entities.add_entity(
             player(self.game.screen_width // 2, self.game.screen_height // 2))
-        print(self.game.entities.id_count)
         self.game.entities.add_entity(ui())
         self.game.entities.add_entity(background_creator())
         self.game.entities.add_entity(asteroid_spawner())
-        print(self.game.entities.id_count)
